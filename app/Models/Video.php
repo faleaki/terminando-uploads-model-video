d6fae4262a555afe9fe4d41ab473febff05dd277
Alexandre <alexandreakio@terra.com.br>
2020-09-25 21:20:59 -0300

    primeiro commit

---
+++ app/Models/Video.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\UploadFiles;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Video extends Model
+{
+    use SoftDeletes, Uuid, UploadFiles;
+
+    const RATING_LIST = ['L', '10', '12', '14', '16', '18'];
+    
+    protected $fillable = [
+        'title',
+        'description',
+        'year_launched',
+        'opened',
+        'rating',
+        'duration',
+        'video_file',
+        'thumb_file'
+    ];
+
+    protected $date = ['delete_at'];
+
+    protected $casts = [
+        'id' => 'string',
+        'opened' => 'boolean',
+        'year_launched' => 'integer',
+        'duration' => 'integer'
+    ];
+    public $incrementing=false;
+    public static $fileFields = [ 'video_file', 'thumb_file' ];
+
+    public static function create(array $attributes = [])
+    {
+        $files = self::extractFiles($attributes);
+        try {
+            \DB::beginTransaction();
+            $obj = static::query()->create($attributes);
+            static::handleRelations($obj, $attributes);
+            $obj->uploadFiles($files);
+            //uploads aqui
+            \DB::commit();
+        } catch(\Exception $e) {
+            if (isset($obj)){
+                //exlcluir os uploads
+                $obj->deleteFiles($files);
+            }
+            \DB::rollBack();
+            throw $e;
+        }
+        return $obj;
+    }
+
+    public function update(array $attributes= [], array $options = [])
+    {
+        $files = self::extractFiles($attributes);
+        try {
+            \DB::beginTransaction();
+            $saved = parent::update($attributes, $options);
+            static::handleRelations($this, $attributes);
+            if ($saved){
+                $this->uploadFiles($files);
+                //excluir os antigos
+            }
+            \DB::commit();
+            if ($saved && count($files)){
+                $this->deleteOldFiles();
+            }
+            return $saved;
+        } catch(\Exception $e) {
+            $this->deleteFiles($files);
+            \DB::rollBack();
+            throw $e;
+        }
+
+    }
+
+    public static function handleRelations(Video $video, array $attributes)
+    {
+        if (isset($attributes['categories_id'])) {
+            $video->categories()->sync($attributes['categories_id']);
+        }
+        if (isset($attributes['genres_id'])) {
+            $video->genres()->sync($attributes['genres_id']);
+        }
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class)->withTrashed();
+    }
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class)->withTrashed();
+    }
+
+    protected function uploadDir()
+    {
+        return $this->id;
+    }
+}
---
+++ tests/Feature/Http/Controllers/Api/GenreControllerTest.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Mockery;
+use App\Http\Controllers\Api\GenreController;
+use App\Models\Genre;
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Tests\Traits\TestSaves;
+use Tests\Traits\TestValidations;
+use Illuminate\Foundation\Testing\TestResponse;
+use Tests\Exceptions\TestException;
+use Illuminate\Http\Request;
+
+class GenreControllerTest extends TestCase
+{
+    use DatabaseMigrations,TestValidations, TestSaves;
+
+    private $genre;
+    private $sendData;
+
+    protected function setUp():void
+    {
+        parent::setUp();
+        $this->genre=factory(Genre::class)->create([
+            'is_active' => true
+        ]);
+        $this->sendData = [
+            'name' => 'name',
+        ];
+    }
+    public function testIndex()
+    {
+        //$genre=factory(Genre::class)->create();
+        $response = $this->get(route('genres.index'));
+        //$genre->refresh();
+        $response
+            ->assertStatus(200)
+            ->assertJson([$this->genre->toArray()]);
+    }
+    public function testShow()
+    {
+        $genre=factory(Genre::class)->create();
+        $response = $this->get(route('genres.show',['genre' => $genre->id]));
+        $genre->refresh();
+        $response
+            ->assertStatus(200)
+            ->assertJson($genre->toArray());
+    }
+
+    public function testInvalidationRequired()
+    {
+        $data = [
+        'name' => '',
+        'categories_id' => '',
+        ];
+        $this->assertInvalidationInStoreAction($data, 'required');
+        $this->assertInvalidationInUpdateAction($data, 'required');
+    }
+
+    public function testInvalidationMax()
+    {
+        $data = [
+            'name' => str_repeat('a', 256),
+        ];
+        $this->assertInvalidationInStoreAction($data, 'max.string', ['max' => 255]);
+        $this->assertInvalidationInUpdateAction($data, 'max.string', ['max' => 255]);
+    }
+
+    public function testInvalidationIsActiveField()
+    {
+        $data = [
+            'is_active' => 's'
+        ];
+        $this->assertInvalidationInStoreAction($data, 'boolean');
+        $this->assertInvalidationInUpdateAction($data, 'boolean');
+    }
+
+    public function testInvalidationCategoriesIdField()
+    {
+        $data = [
+            'categories_id' => 'a'
+        ];
+        $this->assertInvalidationInStoreAction($data, 'array');
+        $this->assertInvalidationInUpdateAction($data, 'array');
+
+        $data = [
+            'categories_id' => [100]
+        ];
+        $this->assertInvalidationInStoreAction($data, 'exists');
+        $this->assertInvalidationInUpdateAction($data, 'exists');
+    }
+
+
+    public function testSave()
+    {
+        $category = factory(Category::class)->create();
+        $data = [
+            [
+                'send_data' => $this->sendData + [
+                    'categories_id' => [$category->id],
+                ],
+                'test_data' => $this->sendData + ['is_active' => true]
+            ],
+            [
+                'send_data' => $this->sendData + [
+                    'is_active' => false,
+                    'categories_id' => [$category->id],
+                ],
+                'test_data' => $this->sendData + [ 'is_active' => false]
+            ]
+        ];
+
+        foreach ($data as $key => $value){
+            $response = $this->assertStore(
+                $value['send_data'],
+                $value['test_data'] + ['deleted_at' => null]
+            );
+            $response->assertJsonStructure([
+                'created_at',
+                'updated_at'
+            ]);
+            $response = $this->assertUpdate(
+                $value['send_data'],
+                $value['test_data'] + ['deleted_at' => null]
+            );
+            $response->assertJsonStructure([
+                'created_at',
+                'updated_at'
+            ]);
+
+            //$categoriesCollection=Genre::find($this->genre->id)->categories->map(function($singlecateg)
+            //{
+            //    return $singlecateg->id;
+            //});
+            //$this->assertEquals(
+            //    $categoriesCollection->toArray(),
+            //    [$category->id]);
+            $this->assertHasCategory($response->json('id'),$category->id);
+        }
+    }
+
+    protected function assertHasCategory($genreId, $categoryId)
+    {
+        $this->assertDatabaseHas('category_genre', [
+            'genre_id' => $genreId,
+            'category_id' => $categoryId
+        ]);
+    }
+
+    public function testRollbackStore()
+    {
+        $controller = \Mockery::mock(GenreController::class)
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+
+        $controller
+            ->shouldReceive('validate')
+            ->withAnyArgs()
+            ->andReturn($this->sendData);
+
+        $controller
+            ->shouldReceive('rulesStore')
+            ->withAnyArgs()
+            ->andReturn([]);
+
+        $controller->shouldReceive('handleRelations')
+            ->once()
+            ->andThrow(new TestException());
+
+        $request = \Mockery::mock(Request::class);
+
+        $hasError = false;
+        try{
+            $controller->store($request);
+        }catch (TestException $exception){
+            $this->assertCount(1, Genre::all());
+            $hasError = true;
+        }
+        $this->assertTrue($hasError);
+    }
+
+    public function testRollbackUpdate()
+    {
+        $controller = \Mockery::mock(GenreController::class)
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+
+        $controller
+            ->shouldReceive('findOrFail')
+            ->withAnyArgs()
+            ->andReturn($this->genre);
+
+        $controller
+            ->shouldReceive('validate')
+            ->withAnyArgs()
+            ->andReturn([
+                'name' => 'teste',
+            ]);
+
+        $controller
+            ->shouldReceive('rulesUpdate')
+            ->withAnyArgs()
+            ->andReturn([]);
+
+        $controller->shouldReceive('handleRelations')
+            ->once()
+            ->andThrow(new TestException());
+
+        $request = \Mockery::mock(Request::class);
+
+        $hasError = false;
+        try{
+            $controller->update($request,1);
+        }catch (TestException $exception){
+            $this->assertCount(1, Genre::all());
+            $hasError = true;
+        }
+        $this->assertTrue($hasError);
+    }
+
+    public function testSyncCategories()
+    {
+        $categoriesId = factory(Category::class, 3)->create()->pluck('id')->toArray();
+        $sendData = [
+            'name' => 'test',
+            'categories_id' => [$categoriesId[0]]
+        ];
+        $response = $this->json('POST', $this->routeStore(), $sendData);
+        $this->assertDatabaseHas('category_genre', [
+            'category_id' => $categoriesId[0],
+            'genre_id' => $response->json('id')
+        ]);
+        $sendData = [
+            'name' => 'test',
+            'categories_id' => [$categoriesId[1], $categoriesId[2]]
+        ];
+        $response = $this->json(
+            'PUT',
+            route('genres.update', [ 'genre' => $response->json('id')]),
+            $sendData
+        );
+        $this->assertDatabaseMissing('category_genre', [
+            'category_id' => $categoriesId[0],
+            'genre_id' => $response->json('id')
+        ]);
+        $this->assertDatabaseHas('category_genre', [
+            'category_id' => $categoriesId[1],
+            'genre_id' => $response->json('id')
+        ]);
+        $this->assertDatabaseHas('category_genre', [
+            'category_id' => $categoriesId[2],
+            'genre_id' => $response->json('id')
+        ]);
+    }
+    public function testDestroy()
+    {
+        $genre=factory(Genre::class)->create();
+        $response = $this->json('DELETE',route('genres.destroy',['genre' => $genre->id]));
+        $response->assertStatus(204);
+        $this->assertNull(Genre::find($genre->id));
+        $this->assertNotNull(Genre::withTrashed()->find($genre->id));
+    }
+
+    protected function model()
+    {
+        return Genre::class;
+    }
+
+    protected function routeStore()
+    {
+        return route('genres.store');
+    }
+
+    protected function routeUpdate()
+    {
+        return route('genres.update', ['genre' => $this->genre->id]);
+    }
+}
